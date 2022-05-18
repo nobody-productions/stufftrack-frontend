ca4815ad111bf81198ad2930284412a6e2622ebe
dag7dev <44711271+dag7dev@users.noreply.github.com>
2022-05-18 23:20:24 +0200

    feat: footer-bar component

---
+++ src/components/footer-bar.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer>
+    <!-- Copyright -->
+    <div class="text-center text-dark p-3">
+        © 2022 Copyright:
+        <a class="text-dark" href="https://github.com/nobody-productions">nobody-productions</a>
+    </div>
+    <!-- Copyright -->
+    </footer>
+
+</template>
+
+<script>
+    export default {
+        name: "footer-bar",
+    }
+</script>
+
+<style scoped>
+    /* import boostrap css */
+    @import url('https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css');
+    @import url('https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.css');
+</style>
