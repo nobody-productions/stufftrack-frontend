cc96b1bede8a43a484be028d97bed7cdd38ce524
dag7dev <dag7@protonmail.com>
2022-05-20 15:33:30 +0200

    enh: add modal profile window

--- src/components/Sidebar-component.vue
+++ src/components/Sidebar-component.vue
@@ -19,17 +19,21 @@
         <li class="nav-item">
           <a class="nav-link" href="#" style="color: black;">
             <i class="fa-solid fa-circle-user" style="height: 20px; width: 20px;"></i>
-            <span class="mx-2">Profilo</span>
+            <span class="mx-2" data-bs-toggle="modal" data-bs-target="#profile-modal">Profilo</span>
           </a>
         </li>
       </ul>
     </div>
   </nav>
+  <profileModal></profileModal>
 </template>
 
 <script>
+import profileModal from '@/components/profile-modal'
+
 export default {
-  name: "Sidebar-component"
+  name: "Sidebar-component",
+  components: { profileModal }
 }
 
 </script>
