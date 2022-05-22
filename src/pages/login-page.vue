deb969e53cf65f410842e8b8551030a667300bc7
Lorenzo Antonelli <lorenzoantonelli@outlook.com>
2022-05-22 19:16:30 +0200

    enh: login error warnings

--- src/pages/login-page.vue
+++ src/pages/login-page.vue
@@ -2,6 +2,7 @@
   <section class="position-relative py-4 py-xl-5">
     <div class="container">
       <div class="row mb-5">
+        <div id="liveAlertPlaceholder"></div>
         <div class="col-md-8 col-xl-6 text-center mx-auto">
           <h2>Accedi a StuffTrack</h2>
           <p class="w-lg-50">Il nuovo modo di tenere traccia dei tuoi interessi e di ciò che ti piace.</p>
@@ -48,22 +49,45 @@ export default {
     });
 
     const router = useRouter();
-    const submit = async() => {
-      await axios.post('login/', {
-        email: form.email,
-        password: form.password,
-      }).then(async () => await router.push("/")).catch( async () => {
-        alert("Credenziali errate");
-      });
-      // dopo il login vai alla pagina principale
 
-
-    };
 
     return {
-      form,
-      submit,
+      form
     };
+  },
+  methods: {
+    showmessage: async function (message, type, time) {
+      const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+      const alert = (message, type) => {
+        const wrapper = document.createElement('div')
+        wrapper.innerHTML = [
+          `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+          `   <div>${message}</div>`,
+          '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+          '</div>'
+        ].join('')
+
+        alertPlaceholder.append(wrapper)
+      }
+
+      alert(message, type)
+      await new Promise(resolve => setTimeout(resolve, time))
+      alertPlaceholder.innerHTML = ''
+    },
+
+     submit: async function() {
+       await axios.post('login/', {
+         email: this.form.email,
+         password: this.form.password,
+       }).then(async () => await router.push("/")).catch(async error => {
+         if (error.message === "Network Error") {
+           await this.showmessage('Network Error', 'danger', 5000)
+         }
+         await this.showmessage(error.response.data.message, 'danger', 5000);
+       });
+      // dopo il login vai alla pagina principale
+    }
   },
   mounted() {
     // check if the user is already logged in
