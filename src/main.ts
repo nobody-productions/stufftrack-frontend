cbc2190049d439666bf0a8b3dae5925a8bc1be9c
Lorenzo Antonelli <lorenzoantonelli@outlook.com>
2022-05-23 13:34:38 +0200

    refactor: removed unused store

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import Vue from 'vue';
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import axios from 'axios'
 
+// configurazione di Axios
 axios.defaults.baseURL = 'http://localhost:8000/api/v1/'
 axios.defaults.withCredentials = true
 
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(router).mount('#app')
