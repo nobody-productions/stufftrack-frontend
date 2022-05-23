ae934f46aaf783e586f875d3d347f3f59c98153c
dag7dev <dag7@protonmail.com>
2022-05-23 04:52:46 +0200

    enh: navbar switches between search books and vg

--- src/components/Nav-dashboard.vue
+++ src/components/Nav-dashboard.vue
@@ -1,30 +1,41 @@
 <template>
   <nav class="navbar navbar-light bg-light p-3">
-    <div class="d-flex col-12 col-md-3 col-lg-2 mb-2 mb-lg-0 flex-wrap flex-md-nowrap justify-content-between align-self-center align-items-center">
+    <div
+      class="d-flex col-12 col-md-3 col-lg-2 mb-2 mb-lg-0 flex-wrap flex-md-nowrap justify-content-between align-self-center align-items-center">
       <a class="navbar-brand align-items-center align-self-center text-center" href="/">
         <i class="fa-solid fa-link"></i>
         StuffTrack
 
       </a>
-      <button class="navbar-toggler d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebar" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler d-md-none collapsed" type="button" data-bs-toggle="collapse"
+        data-bs-target="#sidebar" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
     </div>
     <div class="col-12 col-md-4 col-lg-2 align-self-center align-items-center">
       <div class="input-group">
-        <div class="input-group-append"><button class="btn rounded-pill" type="button" style="padding-left: 0;"><i class="fa fa-search text-secondary"></i></button></div>
-        <span id="search-bar" class="form-control form-control-dark rounded-pill text-secondary" type="text"  aria-label="Search" data-bs-target="#search-modal" data-bs-toggle="modal">Cerca...</span>
+        <div class="input-group-append"><button class="btn rounded-pill" type="button" style="padding-left: 0;"><i
+              class="fa fa-search text-secondary"></i></button></div>
+        
+        <!-- Booleano di controllo: mi servono! Altrimenti non funzionerebbe nulla. -->
+        <span v-if="bkSearch" id="Modal-searchbooks"
+          class="form-control form-control-dark rounded-pill text-secondary" type="text" aria-label="Search"
+          data-bs-target="#modal-searchbooks" data-bs-toggle="modal">Cerca...</span>
+        
+        <span v-else-if="vgSearch" id="search-bar" class="form-control form-control-dark rounded-pill text-secondary" type="text"
+          aria-label="Search" data-bs-target="#search-modal" data-bs-toggle="modal">Cerca...</span>
 
         <!-- Hide this element only on mobile -->
         <div class="dropdown align-items-center align-self-center text-center ps-2  d-md-flex">
-         <i class="fa-solid fa-circle-user fa-2x" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false"></i>
+          <i class="fa-solid fa-circle-user fa-2x" id="dropdownMenuButton" data-bs-toggle="dropdown"
+            aria-expanded="false"></i>
 
-         <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-end" aria-labelledby="dropdownMenuButton">
-           <li><a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#profile-modal">Il mio profilo</a></li>
-           <li><button class="dropdown-item" @click="logout">Log out</button></li>
-         </ul>
+          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-end" aria-labelledby="dropdownMenuButton">
+            <li><a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#profile-modal">Il mio profilo</a></li>
+            <li><button class="dropdown-item" @click="logout">Log out</button></li>
+          </ul>
 
-       </div>
+        </div>
       </div>
     </div>
     <!--
@@ -43,35 +54,65 @@
   </nav>
 
   <searchModal></searchModal>
+  <searchBooks></searchBooks>
 
 </template>
 
 <script>
 import axios from 'axios';
-import {useRouter} from "vue-router";
+import { useRouter } from "vue-router";
 import searchModal from "@/components/search-modal";
+import searchBooks from "@/components/modal-searchbooks";
 
 
 export default {
   name: "Nav-dashboard",
   components: {
-    searchModal
+    searchModal,
+    searchBooks,
+  },
+  data() {
+    return {
+      bkSearch: false,
+      vgSearch: false,
+    };
   },
   setup() {
+    let vgSearch = false;
+    let bkSearch = false;
     let router = useRouter();
     return {
-      logout: function() {
+      logout: function () {
         axios.post('/logout/');
         router.push('/login');
       }
     }
-  }
+  },
+  methods: {
+    enableSearch: function () {
+    // anonymous function
+      if (document.getElementById("side-videogiochi").classList.contains("invert")) {
+        this.vgSearch= true;
+      }
+      else if (document.getElementById("side-libri").classList.contains("invert")) {
+        this.bkSearch = true;
+      }
+    }
+  
+  },
+  mounted() {
+    // aspetto un millisecondo perché altrimenti non funziona (in pratica lui controlla i booleani, e sono sempre a false
+    // perché la pagina non é renderizzata. Lo faccio aspettare 1ms cosi funge.
+    setTimeout(function () { this.enableSearch() }.bind(this), 1)
+}
+
+
 }
 
 </script>
 
 <style scoped>
- /* import boostrap css */
- @import url('https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css');
- @import url('https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.css');
+/* import boostrap css */
+@import url('https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css');
+@import url('https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.css');
 </style>
